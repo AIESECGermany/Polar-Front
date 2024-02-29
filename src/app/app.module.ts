6bcdf3cf5ff8c60268e39a923228c006c9a2fd3d
Ziad Abouelfarah <zain.fake02@gmail.com>
2024-02-29 17:36:00 +0100

    fix: upgrade dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,45 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatCardModule } from '@angular/material/card';
-import { MatDialogModule } from '@angular/material/dialog';
-import { FormsModule } from '@angular/forms';
-import { MatTreeModule } from '@angular/material/tree';
-import { MatIconModule } from '@angular/material/icon';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatListModule } from '@angular/material/list';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { SocialLoginModule, SocialAuthServiceConfig, GoogleLoginProvider } from '@abacritt/angularx-social-login';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HttpClientModule } from "@angular/common/http";
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatSortModule } from "@angular/material/sort";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatCardModule } from "@angular/material/card";
+import { MatDialogModule } from "@angular/material/dialog";
+import { FormsModule } from "@angular/forms";
+import { MatTreeModule } from "@angular/material/tree";
+import { MatIconModule } from "@angular/material/icon";
+import { MatChipsModule } from "@angular/material/chips";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatListModule } from "@angular/material/list";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import {
+  SocialLoginModule,
+  SocialAuthServiceConfig,
+  GoogleLoginProvider,
+} from "@abacritt/angularx-social-login";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { RecruitmentComponent } from './recruitment/recruitment.component';
-import { PerformanceManagementComponent } from './performance-management/performance-management.component';
-import { MembersComponent } from './members/members.component';
-import { RecruiterDetailComponent } from './recruiter-detail/recruiter-detail.component';
-import { LoginComponent } from './login/login.component';
-import { MembersDetailComponent } from './members-detail/members-detail.component';
-import { PerformanceManagementDetailsComponent } from './performance-management-details/performance-management-details.component';
-import { ResponsiveDialogComponent } from './responsive-dialog/responsive-dialog.component';
-import { environment } from 'src/environments/environment.development';
-import { InternalLayoutComponent } from './internal-layout/internal-layout.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SidenavComponent } from "./sidenav/sidenav.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { RecruitmentComponent } from "./recruitment/recruitment.component";
+import { PerformanceManagementComponent } from "./performance-management/performance-management.component";
+import { MembersComponent } from "./members/members.component";
+import { RecruiterDetailComponent } from "./recruiter-detail/recruiter-detail.component";
+import { LoginComponent } from "./login/login.component";
+import { MembersDetailComponent } from "./members-detail/members-detail.component";
+import { PerformanceManagementDetailsComponent } from "./performance-management-details/performance-management-details.component";
+import { ResponsiveDialogComponent } from "./responsive-dialog/responsive-dialog.component";
+import { environment } from "src/environments/environment.development";
+import { InternalLayoutComponent } from "./internal-layout/internal-layout.component";
 
 @NgModule({
   declarations: [
@@ -50,7 +54,7 @@ import { InternalLayoutComponent } from './internal-layout/internal-layout.compo
     MembersDetailComponent,
     PerformanceManagementDetailsComponent,
     ResponsiveDialogComponent,
-    InternalLayoutComponent
+    InternalLayoutComponent,
   ],
   imports: [
     BrowserModule,
@@ -75,28 +79,26 @@ import { InternalLayoutComponent } from './internal-layout/internal-layout.compo
     MatTabsModule,
     MatExpansionModule,
     MatSelectModule,
-    MatSnackBarModule
+    MatSnackBarModule,
   ],
   providers: [
     {
-      provide: 'SocialAuthServiceConfig',
+      provide: "SocialAuthServiceConfig",
       useValue: {
         autoLogin: false,
         providers: [
           {
             id: GoogleLoginProvider.PROVIDER_ID,
-            provider: new GoogleLoginProvider(
-                environment.googleClientId
-              )
-          }
+            provider: new GoogleLoginProvider(environment.googleClientId || ""),
+          },
         ],
         onError: (err) => {
           console.error(err);
-        }
+        },
       } as SocialAuthServiceConfig,
-    }
+    },
   ],
   bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
